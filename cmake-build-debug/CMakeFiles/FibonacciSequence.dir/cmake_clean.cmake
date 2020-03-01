a5de45fb54abb41b63c82397cb170aeef1314d5b
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2020-03-01 21:14:59 +0000

    Fibonacci sequence in C

---
+++ cmake-build-debug/CMakeFiles/FibonacciSequence.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FibonacciSequence.dir/main.c.o"
+  "FibonacciSequence.pdb"
+  "FibonacciSequence"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FibonacciSequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
